063c3e0838e947cf37c619d2161bc4e3a2614f35
kellygalka <kelly3000a1@gmail.com>
2018-08-08 18:13:36 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -11,3 +11,9 @@ end
 def add_colors
   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
 end
+In the file `editing_arrays.rb`, write a solution for the method
+`change_rainbow_colors` that changes the current three values, "yellow",
+"default", and "light_cyan" to the correct colors "red", "light_red",
+"light_yellow". Use the index of these values to make changes.
+
+You should see the rainbow change to these colors when running tests.
